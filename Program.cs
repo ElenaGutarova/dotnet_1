4b367fd35a8f61a0a1846b64e0e5b25da2465fb0
ElenaGutarova <126686800+ElenaGutarova@users.noreply.github.com>
2023-03-07 23:37:13 +0700

    Add files via upload
    Задача 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти максимальное из двух введенных чисел
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32( Console.ReadLine ());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = a;
+if (a > b)
+{
+    Console.WriteLine (max = a);
+    Console.WriteLine (min = b);
+    
+}
+else
+{
+    Console.WriteLine(max = b);
+    Console.WriteLine (min = a);
+}
+Console.Write("max ");
+Console.WriteLine(max);
+Console.Write("min ");
+Console.WriteLine(min);
